5a75d578e509a769a885a9fb7aff10616178faec
Luis Carpio <luisdcm18@gmail.com>
2020-10-12 20:38:19 -0500

    ejercicios

---
+++ condicionales.js
@@ -0,0 +1,15 @@
+const day=new Date().getDay();
+
+switch(day){
+    case 1:
+        console.log('its lunes');
+        break;
+    case 2:
+        console.log('its martes');
+        break;
+    case 3:
+        console.log('its miercoles');
+        break;
+        default:
+            console.log('gg');
+}
